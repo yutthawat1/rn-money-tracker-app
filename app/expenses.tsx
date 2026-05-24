afbdf6e3a13aabdc91fb9c1803e8b931e9999220
yutthawat1 <yutthawat9600@gmail.com>
2026-05-24 20:26:46 +0700

    v1.0.01 (add page and download packages)

---
+++ app/expenses.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Expenses() {
+  return (
+    <View>
+      <Text>Expenses</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ app/welcome.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Welcome() {
+  return (
+    <View>
+      <Text>Welcome</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
