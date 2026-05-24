fe194214cdf6077770ef9b95dafb9d013cd5dedf
yutthawat1 <yutthawat9600@gmail.com>
2026-05-24 21:22:42 +0700

    v1.0.03 (make index page , set layout and add image)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,14 @@
 import { Stack } from "expo-router";
 
+
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="welcome" options={{ headerShown: false }} />
+      <Stack.Screen name="home" options={{ headerShown: false }} />
+      <Stack.Screen name="income" options={{ headerShown: false }} />
+      <Stack.Screen name="expenses" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,52 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { router } from "expo-router";
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View } from 'react-native';
+
 
 export default function Index() {
 
-  // หน่วงเวลา 3 วินาที
-  
+  // หน่วงเวลาหน้าจอ
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.replace("/welcome");
+    }, 3000);
+ 
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <View>
-      <Text>Index</Text>
+    <View  style={styles.container}>
+      <View style={styles.header}>
+        <Text style={{fontSize: 30 , fontWeight: 'bold'}}>Money Tracking</Text>
+        <Text>รายรับ-รายจ่ายของฉัน</Text>
+      </View>
+      
+      <View style={styles.footer}>
+        <Text>Create by 6852D10016</Text>
+        <Text>-SAU-</Text>
+      </View>
     </View>
+    
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#20C92D',
+    alignItems: 'center',
+    justifyContent: 'center',
+    // margin: 10,
+  },
+  header: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  footer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20
+  },
+});
